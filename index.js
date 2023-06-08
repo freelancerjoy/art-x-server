494e6ceab1fd1dd6a821d8908aa5616d103683ae
Md Zihad Shah <freelancerjoy10@gmail.com>
2023-06-09 04:21:32 +0600

    connect mongodb data base and save user name user email

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ArtX server is Running");
+});
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PSASSWORD}@cluster0.dkc5olm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("artxDB").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const quary = { email: user.email };
+      const existUser = await userCollection.findOne(quary);
+      if (existUser){
+        return
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`ArtX server is Running:${port}`);
+});
